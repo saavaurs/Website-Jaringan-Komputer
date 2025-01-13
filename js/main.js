b314d0b566136e77be4a88293a2366a00770e86d
saavaurs <168729371+saavaurs@users.noreply.github.com>
2025-01-13 23:28:26 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const contact = document.querySelectorAll('.nav-link')[2];
+contact.addEventListener('click', () => {
+  alert('Maaf, kami sedang melakukan perbaikan. Halaman contact tidak dapat dilakukan saat ini. Harap coba lagi nanti');
+});
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Website loaded');
+});
+const toggle = document.querySelector('.navbar-toggler');
+const navLink = document.querySelectorAll('.nav-link');
+toggle.addEventListener('click', () => {
+  navLink.forEach(function (nav) {
+    nav.style.color = 'white';
+    nav.style.textAlign = 'left';
+    nav.style.margin = '5px 0px';
+  });
+});
